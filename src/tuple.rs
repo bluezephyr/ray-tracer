14004ac36eb27722ca1c27b2f776b8626b2dc70d
Blue Zephyr <blue.zephyr.git@gmail.com>
2022-09-25 16:19:45 +0200

    Add factory functions for point and vector

--- src/tuple.rs
+++ src/tuple.rs
@@ -6,6 +6,24 @@ struct Tuple {
 }
 
 impl Tuple {
+    pub(crate) fn point(x: f64, y: f64, z: f64) -> Tuple {
+        return Tuple {
+            _x: x,
+            _y: y,
+            _z: z,
+            _w: 1.0,
+        };
+    }
+
+    pub(crate) fn vector(x: f64, y: f64, z: f64) -> Tuple {
+        return Tuple {
+            _x: x,
+            _y: y,
+            _z: z,
+            _w: 0.0,
+        };
+    }
+
     pub(crate) fn is_point(&self) -> bool {
         return self._w == 1.0;
     }
@@ -39,6 +57,20 @@ mod tests {
             _z: 0.0,
             _w: 0.0,
         };
+        assert!(!a.is_point());
+        assert!(a.is_vector());
+    }
+
+    #[test]
+    fn point_method_creates_point() {
+        let a = Tuple::point(1.0, 2.0, 3.0);
+        assert!(a.is_point());
+        assert!(!a.is_vector());
+    }
+
+    #[test]
+    fn vector_method_creates_vector() {
+        let a = Tuple::vector(2.0, 3.0, 4.0);
         assert!(!a.is_point());
         assert!(a.is_vector());
     }
