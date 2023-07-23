964d6fc74f808fd3bbbc17cacbbe61bb882584b9
Blue Zephyr <blue.zephyr.git@gmail.com>
2023-07-23 16:49:48 +0200

    Prevent pixels to be written outside canvas

--- src/canvas.rs
+++ src/canvas.rs
@@ -27,37 +27,55 @@ impl Canvas {
     }
 
     fn write_pixel(&mut self, x: usize, y: usize, color: Color) {
-        let row: &mut Vec<Color> = &mut self.pixels[y];
-        row[x] = color;
+        if x < self.width && y < self.height {
+            let row: &mut Vec<Color> = &mut self.pixels[y];
+            row[x] = color;
+        }
     }
 
-    fn read_pixel(&self, x: usize, y: usize) -> Color {
-        let row = &self.pixels[y];
-        return row[x];
+    fn read_pixel(&self, x: usize, y: usize) -> Option<Color> {
+        if x < self.width && y < self.height {
+            let row = &self.pixels[y];
+            return Some(row[x]);
+        } else {
+            return None;
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    const COLS: usize = 20;
+    const ROWS: usize = 10;
 
     #[test]
     fn canvas_create() {
-        let c = Canvas::create(10, 20);
+        let c = Canvas::create(COLS, ROWS);
 
-        for row in 0..20 {
-            for col in 0..10 {
-                assert!(c.read_pixel(col, row) == Color::color(0.0, 0.0, 0.0));
+        for row in 0..ROWS {
+            for col in 0..COLS {
+                assert!(c.read_pixel(col, row) == Some(Color::color(0.0, 0.0, 0.0)));
             }
         }
     }
 
     #[test]
     fn canvas_write_pixel() {
-        let mut c = Canvas::create(10, 20);
+        let mut c = Canvas::create(COLS, ROWS);
         let red = Color::color(1.0, 0.0, 0.0);
         c.write_pixel(2, 3, red);
-        assert!(c.read_pixel(2, 3) == Color::color(1.0, 0.0, 0.0));
-        assert!(c.read_pixel(2, 2) == Color::color(0.0, 0.0, 0.0));
+        assert!(c.read_pixel(2, 3) == Some(Color::color(1.0, 0.0, 0.0)));
+        assert!(c.read_pixel(2, 2) == Some(Color::color(0.0, 0.0, 0.0)));
+    }
+
+    #[test]
+    fn canvas_write_ignore_if_pixel_outside() {
+        let mut c = Canvas::create(COLS, ROWS);
+        let red = Color::color(1.0, 0.0, 0.0);
+        c.write_pixel(21, 3, red);
+        c.write_pixel(2, 13, red);
+        assert!(c.read_pixel(21, 3) == None);
+        assert!(c.read_pixel(2, 13) == None);
     }
 }
