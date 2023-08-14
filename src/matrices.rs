4ed3d6a0e9a01bf0298d0d6325c6292f476145e5
Blue Zephyr <blue.zephyr.git@gmail.com>
2023-08-14 22:00:55 +0200

    Add matrix multiplication
    The sizes of the matrices that are multiplied needs to be of the same
    size. This is a limitation that needs to be corrected.

--- src/matrices.rs
+++ src/matrices.rs
@@ -1,3 +1,5 @@
+use std::ops::Mul;
+
 pub struct Matrix<const ROWS: usize, const COLS: usize> {
     data: [[f64; COLS]; ROWS],
 }
@@ -10,6 +12,36 @@ impl<const ROWS: usize, const COLS: usize> Matrix<ROWS, COLS> {
 
     fn new_init(data: [[f64; COLS]; ROWS]) -> Self {
         Matrix { data }
+    }
+}
+
+impl<const ROWS: usize, const COLS: usize> PartialEq for Matrix<ROWS, COLS> {
+    fn eq(&self, other: &Self) -> bool {
+        return self.data == other.data;
+    }
+
+    fn ne(&self, other: &Self) -> bool {
+        return !self.eq(other);
+    }
+}
+
+// A * B = M
+impl<const ROWS: usize, const COLS: usize> Mul for Matrix<ROWS, COLS> {
+    type Output = Matrix<ROWS, COLS>;
+
+    fn mul(self, other: Matrix<ROWS, COLS>) -> Matrix<ROWS, COLS> {
+        let mut result = Matrix::<ROWS, COLS>::new();
+
+        // Each col in B will 'generate' a col in M
+        for col in 0..COLS {
+            for row in 0..ROWS {
+                for item in 0..ROWS {
+                    result.data[row][col] =
+                        result.data[row][col] + self.data[row][item] * other.data[item][col]
+                }
+            }
+        }
+        return result;
     }
 }
 
@@ -29,7 +61,7 @@ mod tests {
     }
 
     #[test]
-    fn new_matrix_2_2_init() {
+    fn new_matrix_2x2_init() {
         let m = Matrix::<2, 2>::new_init([[1.0, 2.0], [3.0, 4.0]]);
         assert!(m.data[0][0] == 1.0);
         assert!(m.data[0][1] == 2.0);
@@ -38,7 +70,7 @@ mod tests {
     }
 
     #[test]
-    fn new_matrix_3_3_init() {
+    fn new_matrix_3x3_init() {
         let m = Matrix::<3, 3>::new_init([[-3.0, 5.0, 0.0], [1.0, -2.0, -7.0], [0.0, 1.0, 1.0]]);
         assert!(m.data[0][0] == -3.0);
         assert!(m.data[1][1] == -2.0);
@@ -46,7 +78,7 @@ mod tests {
     }
 
     #[test]
-    fn new_matrix_4_4_init() {
+    fn new_matrix_4x4_init() {
         let m = Matrix::<4, 4>::new_init([
             [1.0, 2.0, 3.0, 4.0],
             [5.5, 6.5, 7.5, 8.5],
@@ -60,5 +92,64 @@ mod tests {
         assert!(m.data[2][2] == 11.0);
         assert!(m.data[3][0] == 13.5);
         assert!(m.data[3][2] == 15.5);
+    }
+
+    #[test]
+    fn matrices_identical() {
+        let a = Matrix::<4, 4>::new_init([
+            [1.0, 2.0, 3.0, 4.0],
+            [5.0, 6.0, 7.0, 8.0],
+            [9.0, 10.0, 11.0, 12.0],
+            [13.0, 14.0, 15.0, 16.0],
+        ]);
+        let b = Matrix::<4, 4>::new_init([
+            [1.0, 2.0, 3.0, 4.0],
+            [5.0, 6.0, 7.0, 8.0],
+            [9.0, 10.0, 11.0, 12.0],
+            [13.0, 14.0, 15.0, 16.0],
+        ]);
+        assert!(a == b);
+    }
+
+    #[test]
+    fn matrices_diffrent() {
+        let a = Matrix::<4, 4>::new_init([
+            [5.0, 6.0, 7.0, 8.0],
+            [9.0, 10.0, 11.0, 12.0],
+            [13.0, 14.0, 15.0, 16.0],
+            [1.0, 2.0, 3.0, 4.0],
+        ]);
+        let b = Matrix::<4, 4>::new_init([
+            [1.0, 2.0, 3.0, 4.0],
+            [5.0, 6.0, 7.0, 8.0],
+            [9.0, 10.0, 11.0, 12.0],
+            [13.0, 14.0, 15.0, 16.0],
+        ]);
+        assert!(a != b);
+    }
+
+    #[test]
+    fn multiply_4x4_matrices() {
+        let a = Matrix::<4, 4>::new_init([
+            [1.0, 2.0, 3.0, 4.0],
+            [5.0, 6.0, 7.0, 8.0],
+            [9.0, 8.0, 7.0, 6.0],
+            [5.0, 4.0, 3.0, 2.0],
+        ]);
+        let b = Matrix::<4, 4>::new_init([
+            [-2.0, 1.0, 2.0, 3.0],
+            [3.0, 2.0, 1.0, -1.0],
+            [4.0, 3.0, 6.0, 5.0],
+            [1.0, 2.0, 7.0, 8.0],
+        ]);
+        assert!(
+            a * b
+                == Matrix::<4, 4>::new_init([
+                    [20.0, 22.0, 50.0, 48.0],
+                    [44.0, 54.0, 114.0, 108.0],
+                    [40.0, 58.0, 110.0, 102.0],
+                    [16.0, 26.0, 46.0, 42.0],
+                ])
+        );
     }
 }
