c4552688f758a1aafe3e42391b6f4ac61eb893e2
Blue Zephyr <blue.zephyr.git@gmail.com>
2023-08-21 21:10:08 +0200

    Add submatrix methods for 3x3 and 4x4 matrices

--- src/matrices.rs
+++ src/matrices.rs
@@ -36,6 +36,12 @@ impl<const R: usize, const C: usize> Matrix<R, C> {
     }
 }
 
+// impl<const R: usize, const C: usize, const P: usize, const Q: usize> Matrix<R, C> {
+//     fn submatrix(&self, row: usize, col: usize) -> Matrix<P, Q> {
+//         return Matrix::new<P, Q>();
+//     }
+// }
+
 // The methods below are only applicable on square matrices
 impl<const R: usize> Matrix<R, R> {
     // Create an identity Matrix
@@ -99,6 +105,69 @@ impl Mul<&Tuple> for &Matrix<4, 4> {
     }
 }
 
+// The determinant is simple to calculate for 2x2 matrices
+impl Matrix<2, 2> {
+    fn det(self) -> f64 {
+        let d = self.data;
+        return d[0][0] * d[1][1] - d[0][1] * d[1][0];
+    }
+}
+
+// Since feature(generic_const_exprs) is experimental (see https://github.com/rust-lang/rust/issues/76560)
+// there is currently no generic way to implement certain operations. Thus, these needs to be
+// specific.
+impl Matrix<3, 3> {
+    fn submatrix_3x3(&self, row: usize, col: usize) -> Option<Matrix<2, 2>> {
+        if row > 2 || col > 2 {
+            return None;
+        }
+
+        let mut s = Matrix::<2, 2>::new();
+        let mut out_row = 0;
+
+        for r in 0..3 {
+            let mut out_col = 0;
+            if r != row {
+                for c in 0..3 {
+                    if c != col {
+                        s.data[out_row][out_col] = self.data[r][c];
+                        out_col = out_col + 1;
+                    }
+                }
+                out_row = out_row + 1;
+            }
+        }
+
+        return Some(s);
+    }
+}
+
+impl Matrix<4, 4> {
+    fn submatrix_4x4(&self, row: usize, col: usize) -> Option<Matrix<3, 3>> {
+        if row > 3 || col > 3 {
+            return None;
+        }
+
+        let mut s = Matrix::<3, 3>::new();
+        let mut out_row = 0;
+
+        for r in 0..4 {
+            let mut out_col = 0;
+            if r != row {
+                for c in 0..4 {
+                    if c != col {
+                        s.data[out_row][out_col] = self.data[r][c];
+                        out_col = out_col + 1;
+                    }
+                }
+                out_row = out_row + 1;
+            }
+        }
+
+        return Some(s);
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -122,7 +191,6 @@ mod tests {
         assert!(m.data[1][0] == 3.0);
         assert!(m.data[1][1] == 4.0);
     }
-
     #[test]
     fn new_matrix_3x3_init() {
         let m = Matrix::<3, 3>::new_init([[-3.0, 5.0, 0.0], [1.0, -2.0, -7.0], [0.0, 1.0, 1.0]]);
@@ -313,5 +381,67 @@ mod tests {
     fn transpose_identity_matrix_returns_identity_matrix() {
         let identity = Matrix::<3, 3>::new_identity();
         assert!(identity.transpose() == identity);
+    }
+
+    #[test]
+    fn calculate_determinant_for_2x2_matrix() {
+        let m = Matrix::new_init([
+            [1.0, 5.0],  //
+            [-3.0, 2.0], //
+        ]);
+        assert!(m.det() == 17.0);
+    }
+
+    #[test]
+    fn calculate_submatrix_for_3x3_matrix() {
+        let m = Matrix::new_init([
+            [1.0, 5.0, 0.0],  //
+            [-3.0, 2.0, 7.0], //
+            [0.0, 6.0, -3.0], //
+        ]);
+        assert_eq!(m.submatrix_3x3(3, 0), None);
+        assert_eq!(m.submatrix_3x3(0, 3), None);
+        assert_eq!(
+            m.submatrix_3x3(0, 2),
+            Some(Matrix::new_init([
+                [-3.0, 2.0], //
+                [0.0, 6.0],  //
+            ]))
+        );
+        assert_eq!(
+            m.submatrix_3x3(2, 1),
+            Some(Matrix::new_init([
+                [1.0, 0.0],  //
+                [-3.0, 7.0], //
+            ]))
+        );
+    }
+
+    #[test]
+    fn calculate_subnatrix_for_4x4_matrix() {
+        let m = Matrix::new_init([
+            [-6.0, 1.0, 1.0, 6.0],
+            [-8.0, 5.0, 8.0, 6.0],
+            [-1.0, 0.0, 8.0, 2.0],
+            [-7.0, 1.0, -1.0, 1.0],
+        ]);
+        assert_eq!(m.submatrix_4x4(4, 0), None);
+        assert_eq!(m.submatrix_4x4(0, 4), None);
+        assert_eq!(
+            m.submatrix_4x4(2, 1),
+            Some(Matrix::new_init([
+                [-6.0, 1.0, 6.0],
+                [-8.0, 8.0, 6.0],
+                [-7.0, -1.0, 1.0],
+            ]))
+        );
+        assert_eq!(
+            m.submatrix_4x4(0, 3),
+            Some(Matrix::new_init([
+                [-8.0, 5.0, 8.0],
+                [-1.0, 0.0, 8.0],
+                [-7.0, 1.0, -1.0],
+            ]))
+        );
     }
 }
